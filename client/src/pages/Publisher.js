ad7afc37eb9e859a4a54953f2327be69693e4041
malpanisukrit <malpanisukrit@gmail.com>
2023-08-20 19:06:33 +0530

    Resolved Styling Issues

--- client/src/pages/Publisher.js
+++ client/src/pages/Publisher.js
@@ -18,23 +18,11 @@ export default function Publisher() {
 
   const [messages, setMesages] = useState([]);
   const inputRef = useRef(null);
-  const user1 = JSON.parse(localStorage.getItem("user"))
-  const name = user1?.email.split("@")[0]
+  let name = JSON.parse(localStorage.getItem("user")).name
   const socket = io("http://localhost:5001")
 
   useEffect(() => {
     return () => {
-      axios
-        .get("http://localhost:3001/chats/64e0f4dd94fe7308aa27db3a")
-        .then((result) => {
-          // console.log(result.data.chat);
-          let arr = [];
-          result.data.chat.map((item) => {
-            arr.push(item);
-          });
-
-          setMesages(arr);
-        });
       socket.on("connect", () => {
         console.log("Connected to Socket.io Server");
       });
@@ -44,7 +32,7 @@ export default function Publisher() {
 
   socket.on("receive-chat-message", (data) => {
     setMesages([...messages, data]);
-    // console.log(data);
+    console.log(data);
   });
 
   const handleSubmit = (e) => {
@@ -67,22 +55,22 @@ export default function Publisher() {
     }
   };
 
-  const handleLiveChats = async()=>{
+  const handleLiveChats = async () => {
     console.log("working")
 
     const resp = await axios.delete("http://localhost:3001/chats/64e0f4dd94fe7308aa27db3a")
     console.log(resp)
 
     axios
-    .get("http://localhost:3001/chats/64e0f4dd94fe7308aa27db3a")
-    .then((result) => {
-      console.log(result.data.chat);
-      let arr = [];
-      result.data.chat.map((item) => {
-        arr.push(item);
+      .get("http://localhost:3001/chats/64e0f4dd94fe7308aa27db3a")
+      .then((result) => {
+        console.log(result.data.chat);
+        let arr = [];
+        result.data.chat.map((item) => {
+          arr.push(item);
+        });
+        setMesages(arr);
       });
-      setMesages(arr);
-    });
   }
 
   async function init() {
@@ -150,7 +138,7 @@ export default function Publisher() {
   return (
     <div>
       <Navbar />
-      <div className="flex   bg-[#44455B] flex-row min-h-screen justify-center items-center h-full">
+      <div className="flex  bg-[#44455B] flex-row min-h-screen justify-center items-center h-full">
         <div className="flex rounded-lg p-5 flex-row min-h-screen min-w-full justify-around items-center h-full">
           <div className=" bg-gradient-to-r m-5 rounded-lg from-purple-800 to-blue-700 p-4 w-10/12 h-full">
             <div className="rounded-lg bg-white p-2">
@@ -167,7 +155,7 @@ export default function Publisher() {
                 disabled={loading}
                 className="mt-4 h-10 w-32 flex flex-row justify-around items-center ml-20 mr-20 bg-[#16a085] hover:bg-[#27ae60] hover:scale-110 duration-300 text-white py-2 px-4 rounded-full"
                 id="my-button"
-                onClick={()=>{
+                onClick={() => {
                   init();
                   handleLiveChats();
                 }}
@@ -233,4 +221,4 @@ export default function Publisher() {
       <Footer />
     </div>
   );
-  }
+}
