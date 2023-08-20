0120b45d2eb0628975de4666afa6ddebdc29e99c
harshs0891 <harshs0891@gmail.com>
2023-08-20 20:50:56 +0530

    added logo to login and signup page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSignup } from '../hooks/useSignup';
+import logo from '../images/logo.png'
 export default function Register() {
     const navigate = useNavigate();
     const [formData, setFormData] = useState({ name: "", email: "", password: "" });
@@ -21,35 +22,6 @@ export default function Register() {
         <div>
             <Navbar />
             <main className="w-full flex bg-white">
-                <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
-                    <div className="relative z-10 w-full max-w-md">
-                        <img src="https://floatui.com/logo-dark.svg" width={150} />
-                        <div className=" mt-16 space-y-3">
-                            <h3 className="text-white text-3xl font-bold">Start growing your business quickly</h3>
-                            <p className="text-gray-300">
-                                Create an account and get access to all features for 30-days, No credit card required.
-                            </p>
-                            <div className="flex items-center -space-x-2 overflow-hidden">
-                                <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://randomuser.me/api/portraits/men/86.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <p className="text-sm text-gray-400 font-medium translate-x-5">
-                                    Join 5.000+ users
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="absolute inset-0 my-auto h-[500px]"
-                        style={{
-                            background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", filter: "blur(118px)"
-                        }}
-                    >
-
-                    </div>
-                </div>
                 <div className="flex-1 flex items-center justify-center h-screen">
                     <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
                         <div className="">
@@ -106,7 +78,7 @@ export default function Register() {
                         </div>
                         <form
                             onSubmit={(e) => e.preventDefault()}
-                            className="space-y-5"
+                            className="space-y-5 border-2 p-5 rounded-lg shadow-lg"
                         >
                             <div>
                                 <label className="font-medium">
